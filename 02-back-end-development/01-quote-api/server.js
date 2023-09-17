34af1ab5d95d5d421ea1d952d41442f7c28367cf
Edwin <starlest@gmail.com>
2023-09-17 22:39:20 +0700

    complete quote api project

--- 02-back-end-development/01-quote-api/server.js
+++ 02-back-end-development/01-quote-api/server.js
@@ -1,10 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
+
+app.get("/api/quotes", (req, res) => {
+  const person = req.query.person;
+  if (person) {
+    res.send({
+      quotes: quotes.filter((q) => q.person == person),
+    });
+  } else {
+    res.send({
+      quotes: quotes,
+    });
+  }
+});
+
+app.get("/api/quotes/random", (req, res) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({
+    quote: randomQuote,
+  });
+});
+
+app.post("/api/quotes", (req, res) => {
+  const quote = req.query.quote;
+  const person = req.query.person;
+  if (quote && person) {
+    const newQuote = {
+      quote: quote,
+      person: person,
+    };
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.listen(PORT);
