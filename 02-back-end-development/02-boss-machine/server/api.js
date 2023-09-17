eb5ecde951a166e3d91394f994c4d0f7ea829366
Edwin Chia <starlest@gmail.com>
2023-09-18 03:00:24 +0700

    add cors, body-parser, and api router

--- 02-back-end-development/02-boss-machine/server/api.js
+++ 02-back-end-development/02-boss-machine/server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
+const {getAllFromDatabase} = require('./db.js');
 
-
+apiRouter.get('/minions', (req, res, next) => {
+    console.log('hi minions');
+    res.send(getAllFromDatabase('minions'));
+});
 
 module.exports = apiRouter;
